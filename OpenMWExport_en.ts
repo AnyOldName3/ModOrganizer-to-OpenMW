142b32184a6ea58e2fa81e56178c3086c29823b7
AnyOldName3 <krizdjali+github@gmail.com>
2018-04-12 23:20:36 +0100

    Actually add the .ts file.

---
+++ OpenMWExport_en.ts
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>OpenMWExportPlugin</name>
+    <message>
+        <location filename="OpenMWExport.py" line="37"/>
+        <source>Transfers mod list (left pane) to data fields in OpenMW.cfg and plugin list (right pane, plugins tab) to content fields in OpenMW.cfg. This allows you to run OpenMW with the current profile&apos;s setup from outside of Mod Organizer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="OpenMWExport.py" line="49"/>
+        <source>Export to OpenMW</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="OpenMWExport.py" line="52"/>
+        <source>Exports the current mod list and plugin load order to OpenMW.cfg</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="OpenMWExport.py" line="65"/>
+        <source>Incompatible game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="OpenMWExport.py" line="65"/>
+        <source>(At least when this plugin is being written) OpenMW only supports game data designed for the Morrowind engine. The game being managed is not Morrowind, so the export will abort. If you think you know better than this message, update this plugin.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="OpenMWExport.py" line="68"/>
+        <source>Before starting export...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="OpenMWExport.py" line="68"/>
+        <source>Before starting the export to OpenMW, please ensure you&apos;ve backed up anything in OpenMW.cfg which you do not want to risk losing forever.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="OpenMWExport.py" line="74"/>
+        <source>Config file not specified</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="OpenMWExport.py" line="74"/>
+        <source>No config file was specified</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="OpenMWExport.py" line="96"/>
+        <source>OpenMW Export Complete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="OpenMWExport.py" line="96"/>
+        <source>The export to OpenMW completed successfully. The current setup was saved to {0}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="OpenMWExport.py" line="147"/>
+        <source>Locate OpenMW Config File</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
