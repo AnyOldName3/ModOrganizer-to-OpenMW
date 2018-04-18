ac02c75260af76fcdecc94a6b44cd24c5a2f11fe
AnyOldName3 <krizdjali+github@gmail.com>
2018-04-18 14:54:06 +0100

    Fix typo causing a crash when displaying the success message. Update version to 1.1.1

--- OpenMWExport.py
+++ OpenMWExport.py
@@ -37,7 +37,7 @@ class OpenMWExportPlugin(mobase.IPluginTool):
         return self.__tr("Transfers mod list (left pane) to data fields in OpenMW.cfg and plugin list (right pane, plugins tab) to content fields in OpenMW.cfg. This allows you to run OpenMW with the current profile's setup from outside of Mod Organizer")
 
     def version(self):
-        return mobase.VersionInfo(1, 1, 0, mobase.ReleaseType.final)
+        return mobase.VersionInfo(1, 1, 1, mobase.ReleaseType.final)
 
     def isActive(self):
         return True
@@ -93,7 +93,7 @@ class OpenMWExportPlugin(mobase.IPluginTool):
             # actually write out the list
             for pluginIndex in range(len(loadOrder)):
                 openmwcfg.write(u"content=" + loadOrder[pluginIndex] + u"\n")
-        QMessageBox.information(self.__parentWidget, self.__tr("OpenMW Export Complete"), self.__tr("The export to OpenMW completed successfully. The current setup was saved to {0}"),format(configPath))
+        QMessageBox.information(self.__parentWidget, self.__tr("OpenMW Export Complete"), self.__tr("The export to OpenMW completed successfully. The current setup was saved to {0}").format(configPath))
     
     def __tr(self, str):
         return QCoreApplication.translate("OpenMWExportPlugin", str)
