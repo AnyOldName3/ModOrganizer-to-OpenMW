9c43cd57e50f846cbd52155e86ed33a539468146
AnyOldName3 <krizdjali+gitlab@gmail.com>
2023-02-19 23:46:54 +0000

    Minimal changes for PyQt6

--- OpenMWExport_en.ts
+++ OpenMWExport_en.ts
@@ -1,91 +1,92 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0">
-<context>
+<!DOCTYPE TS>
+<TS version="2.0">
+  <context>
     <name>OpenMWExportPlugin</name>
     <message>
-        <location filename="OpenMWExport.py" line="45"/>
-        <source>Transfers mod list (left pane) to data fields in OpenMW.cfg and plugin list (right pane, plugins tab) to content fields in OpenMW.cfg. This allows you to run OpenMW with the current profile&apos;s setup from outside of Mod Organizer</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="29" />
+      <source>OpenMWExport plugin requires a Python {0} interpreter or newer, but is running on a Python {1} interpreter.</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="62"/>
-        <source>Export to OpenMW</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="30" />
+      <source>Incompatible Python version.</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="65"/>
-        <source>Exports the current mod list and plugin load order to OpenMW.cfg</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="30" />
+      <source>This version of the OpenMW Export plugin requires a Python {0} interpreter or newer, but Mod Organizer has provided a Python {1} interpreter. Mod Organizer {2} is the earliest compatible version. You should check for an update.</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="83"/>
-        <source>Incompatible game</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="45" />
+      <source>Transfers mod list (left pane) to data fields in OpenMW.cfg and plugin list (right pane, plugins tab) to content fields in OpenMW.cfg. This allows you to run OpenMW with the current profile's setup from outside of Mod Organizer</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="83"/>
-        <source>(At least when this plugin is being written) OpenMW only supports game data designed for the Morrowind engine. The game being managed is not Morrowind, so the export will abort. If you think you know better than this message, update this plugin.</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="62" />
+      <source>Export to OpenMW</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="86"/>
-        <source>Before starting export...</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="65" />
+      <source>Exports the current mod list and plugin load order to OpenMW.cfg</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="86"/>
-        <source>Before starting the export to OpenMW, please ensure you&apos;ve backed up anything in OpenMW.cfg which you do not want to risk losing forever.</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="83" />
+      <source>Incompatible game</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="92"/>
-        <source>Config file not specified</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="83" />
+      <source>(At least when this plugin is being written) OpenMW only supports game data designed for the Morrowind engine. The game being managed is not Morrowind, so the export will abort. If you think you know better than this message, update this plugin.</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="92"/>
-        <source>No config file was specified</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="86" />
+      <source>Before starting export...</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="113"/>
-        <source>OpenMW Export Complete</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="86" />
+      <source>Before starting the export to OpenMW, please ensure you've backed up anything in OpenMW.cfg which you do not want to risk losing forever.</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="113"/>
-        <source>The export to OpenMW completed successfully. The current setup was saved to {0}</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="92" />
+      <source>Config file not specified</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="166"/>
-        <source>Locate OpenMW Config File</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="92" />
+      <source>No config file was specified</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="29"/>
-        <source>OpenMWExport plugin requires a Python {0} interpreter or newer, but is running on a Python {1} interpreter.</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="113" />
+      <source>OpenMW Export Complete</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="30"/>
-        <source>Incompatible Python version.</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="113" />
+      <source>The export to OpenMW completed successfully. The current setup was saved to {0}</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="30"/>
-        <source>This version of the OpenMW Export plugin requires a Python {0} interpreter or newer, but Mod Organizer has provided a Python {1} interpreter. Mod Organizer {2} is the earliest compatible version. You should check for an update.</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="167" />
+      <source>Locate OpenMW Config File</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="174"/>
-        <source>Plugin update available</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="175" />
+      <source>Plugin update available</source>
+      <translation type="unfinished" />
     </message>
     <message>
-        <location filename="OpenMWExport.py" line="174"/>
-        <source>{0} can be updated from version {1} to {2}. Do you want to open the download page in your browser?</source>
-        <translation type="unfinished"></translation>
+      <location filename="OpenMWExport.py" line="175" />
+      <source>{0} can be updated from version {1} to {2}. Do you want to open the download page in your browser?</source>
+      <translation type="unfinished" />
     </message>
-</context>
+  </context>
 </TS>
